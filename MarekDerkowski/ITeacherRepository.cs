c9efac764160c137ae8616db94646d4f9faeaad6
DESKTOP-UPH3JIK\Marek <m.derkowski@gmail.com>
2019-05-29 10:51:12 +0200

    add TeacherRepository with interfaces

---
+++ MarekDerkowski/ITeacherRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarekDerkowski
+{
+    interface ITeacherRepository: IRepository<Teacher>
+    {
+        void AddLesson(Lesson lesson);
+        void RemoveLesson(Lesson lesson);
+    }
+}
---
+++ MarekDerkowski/TeacherRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarekDerkowski
+{
+    public class TeacherRepository : ITeacherRepository
+    {
+        private Teacher teacher = null;
+        public void Add(Teacher item)
+        {
+            teacher = item;
+        }
+
+        public void AddLesson(Lesson lesson)
+        {
+            teacher.Lessons.Add(lesson);
+        }
+
+        public Teacher Get()
+        {
+            return teacher;
+        }
+
+        public void Remove(Teacher item)
+        {
+            teacher = null;
+        }
+
+        public void RemoveLesson(Lesson lesson)
+        {
+            teacher.Lessons.Remove(lesson);
+        }
+
+        public void Update(Teacher item)
+        {
+            teacher = item;
+        }
+
+
+    }
+}
